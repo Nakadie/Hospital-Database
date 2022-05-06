5ea5bfe5c0dadd1ec6b5491b39195ee06b9759bd
Nakadie <77523003+Nakadie@users.noreply.github.com>
2022-05-06 17:14:20 +0900

    implemented list all functionality

--- Database-code/testbranch/test_models.py
+++ Database-code/testbranch/test_models.py
@@ -1,5 +1,33 @@
 import unittest
 import models
+import views
 
-class test_models(unittest.TestCase):
-    pass
+class TestBasic(unittest.TestCase):
+    def setUp(self):
+        # Load test data
+        self.app = App(database='fixtures/test_basic.json')
+
+    def test_customer_count(self):
+        self.assertEqual(len(self.app.customers), 100)
+
+    def test_existence_of_customer(self):
+        customer = self.app.get_customer(id=10)
+        self.assertEqual(customer.name, "Org XYZ")
+        self.assertEqual(customer.address, "10 Red Road, Reading")
+
+
+class TestComplexData(unittest.TestCase):
+    def setUp(self):
+        # load test data
+        self.app = App(database='fixtures/test_complex.json')
+
+    def test_customer_count(self):
+        self.assertEqual(len(self.app.customers), 10000)
+
+    def test_existence_of_customer(self):
+        customer = self.app.get_customer(id=9999)
+        self.assertEqual(customer.name, u"バナナ")
+        self.assertEqual(customer.address, "10 Red Road, Akihabara, Tokyo")
+
+if __name__ == '__main__':
+    unittest.main()
